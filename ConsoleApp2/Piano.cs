9a2ee0128023d8cf8ece32cbc29230ed1007ab11
Fábio Garcia Rios <fabiogarcia45@gmail.com>
2022-06-27 15:58:34 -0300

    Adicionada classe Piano

---
+++ ConsoleApp2/Piano.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Piano : Instrument
+    {
+
+        public int NumKeys { get; set; }
+        public bool Acoustic { get; set; }
+        public bool Pedals { get; set; }
+        public string PianoType { get; set; }
+
+    }
+}
